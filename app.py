fd03d00649911bc4f05f3709d735db4bd96a1ee0
Manuel Procacci <maprocaccivalero@gmail.com>
2024-11-18 13:56:19 -0500

    Pequeño proyecto en Flask, Lo que hace es bsuacr el clima de una ciudad que se solicita en las unidades Metricas o Americanas (Imperiales).

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from clima import obtener_clima
+
+# Crea la instancia de la aplicación Flask
+app = Flask(__name__)
+
+# Define la ruta raíz de la aplicación, que muestra un mensaje de bienvenida
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Define la ruta para la consulta del clima
+@app.route('/weather', methods=['POST'])
+def weather():
+    """
+    Obtiene el clima de una ciudad y las unidades de medida.
+    :return: Devuelve una plantilla HTML con los resultados del clima.
+    """
+
+    # Obtiene los datos de la petición POST y obtiene la ciudad y las unidades de medida.
+    ciudad = request.form['city']
+    units = request.form['units']
+
+    # Obtiene el clima de la ciudad y las unidades de medida. Si la ciudad no es encontrada, muestra un error.
+    clima_data = obtener_clima(ciudad, units)
+    if clima_data is None:
+        return render_template('index.html', error='Ciudad no encontrada')
+    else:
+        return render_template('weather.html', ciudad=ciudad, clima=clima_data, units=units)
+
+# Ejecuta la aplicación en el puerto 5000. El argumento debug=True habilita la depuración de la aplicación.
+if __name__ == '__main__':
+    """
+    Este código se ejecuta cuando se ejecuta este script directamente. En caso de ser importado como un módulo, este código no se ejecuta.
+    Este script se encarga de inicializar la aplicación Flask y exponerla en el puerto 5000.
+    """
+    app.run(port=5000, debug=True)
+
---
+++ clima.py
@@ -0,0 +1,65 @@
+import requests
+
+def obtener_clima(ciudad, units='metric'):
+    """Devuelve la temperatura actual en grados Celsius de la ciudad pasada como argumento.
+
+    Args:
+        ciudad (str): Nombre de la ciudad.
+        units (str): Sistema de medición ('metric' para Celsius, 'imperial' para Fahrenheit).
+    Returns:
+        dict: Diccionario con la temperatura, la temperatura maxima, la temperatura minima,
+        la presion, la velocidad del viento, la humedad y la descripcion del clima.
+    """
+    # Creamos la URL base de la API de OpenWeatherMap.
+    base_url = f"http://api.openweathermap.org/data/2.5/weather?"
+    # Agregamos la clave de la API de OpenWeatherMap al URL.
+    api_key = '3796934179af73efc7d5213b80cda195'
+    # Agregamos la API key al URL para obtener los datos del clima.
+    # El parámetro 'lang' se utiliza para especificar el idioma de la respuesta.
+    # El parámetro 'units' se utiliza para especificar la unidad de medida (metric, imperial o standard).
+    full_url = f"{base_url}q={ciudad}&lang=es&appid={api_key}&units={units}"
+     # Realizamos la peticion HTTP y obtenemos la respuesta.
+    response = requests.get(full_url)
+
+    if response.status_code == 200:
+        # 200 es el codigo de respuesta correcto de la peticion HTTP y 404 es el codigo de error.
+
+        # Obtenemos los datos del clima del JSON de la respuesta.
+        data = response.json()
+        # Obtenemos los datos principales del clima.
+        main = data['main']
+        # Obtenemos los datos del viento.
+        wind = data['wind']
+        # Obtenemos la descripción del clima.
+        weather_description = data['weather'][0]['description']
+
+        # Convertir presión de hPa a inHg si el sistema de medición es imperial
+        presion = main['pressure']
+        if units == 'imperial':
+            presion = round(presion * 0.02953, 2)
+
+        # Convertir velocidad del viento de m/s a Km/h si el sistema de medición es metric
+        velocidad_viento = wind['speed']
+        if units == 'metric':
+            velocidad_viento = round(velocidad_viento * 3.6, 2)
+
+        # Devolvemos el diccionario con los datos del clima.
+        return {
+            'temperatura': main['temp'],
+            'temperatura_maxima': main['temp_max'],
+            'temperatura_minima': main['temp_min'],
+            'sensacion_termica': main['feels_like'],
+            'presion': presion,
+            'velocidad_viento': velocidad_viento,
+            'humedad': main['humidity'],
+            'descripcion': weather_description,
+        }
+    else:
+        return None
+
+# Ejemplo de uso:
+"""
+# Obtenemos el clima de Buenos Aires.
+weather = obtener_clima('Buenos Aires')
+print(weather)
+"""
